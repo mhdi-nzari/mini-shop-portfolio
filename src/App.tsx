0680740d6547322eae3df5ede28712aea4bdfe1a
mhdi_nzari <mahdi.nazari.gb008@gmail.com>
2023-04-29 16:13:08 +0330

    fix bux in other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
 import NavBar from "./components/NavBar";
